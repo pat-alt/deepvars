c97576f890ac422ad4dd818b1b4c350238830e6a
pat-alt <altmeyerpat@gmail.com>
2021-04-28 08:18:44 +0200

    small change

--- dev/01_start.R
+++ dev/01_start.R
@@ -1,16 +1,3 @@
-# Building a Prod-Ready, Robust Shiny Application.
-#
-# README: each step of the dev files is optional, and you don't have to
-# fill every dev scripts before getting started.
-# 01_start.R should be filled at start.
-# 02_dev.R should be used to keep track of your development during the project.
-# 03_deploy.R should be used once you need to deploy your app.
-#
-#
-########################################
-#### CURRENT FILE: ON START SCRIPT #####
-########################################
-
 usethis::use_mit_license( name = "SVAA User" )  # You can set another license here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_lifecycle_badge( "Experimental" )
