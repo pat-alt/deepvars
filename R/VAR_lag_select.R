36ad5340adaab388086f74866b55ba7930d9fdda
pat-alt <altmeyerpat@gmail.com>
2021-06-02 11:09:21 +0000

    updated VAR_lag_select function

--- R/VAR_lag_select.R
+++ R/VAR_lag_select.R
@@ -12,46 +12,26 @@
 VAR_lag_select = function(data, max_lag=10, ic_choice="AIC") {
 
   # Run loop ----
-  output = lapply(1:max_lag, function(i) {
+  output <- lapply(
+    1:max_lag,
+    function(m) {
+      var_model <- vareg(data, lags=m)
+      N <- var_model$model_data$N
+      K <- var_model$model_data$K
+      res <- var_model$res
+      Sigma_res = crossprod(res)/N
+      AIC = log(det(Sigma_res)) + (2/N) * ((K ** 2) * m + K) # Akaike
+      HQC = log(det(Sigma_res)) + ((2 * log(log(N))) / N) * ((K ** 2) * m + K) # Hannan-Quinn
+      SIC = log(det(Sigma_res)) + (log(N)/(N)) * ((K ** 2) * m + K) # Schwarze/Bayes
+      criteria = data.table::data.table(
+        m = m,
+        SIC = SIC,
+        HQC = HQC,
+        AIC = AIC
+      )
 
-    lag = i
-
-    # Preprocessing ----
-    N = nrow(data)-lag
-    K = ncol(data)
-    var_names = colnames(data)
-    data_out = data.table::as.data.table(data)
-    # Data and lags:
-    data_out[,(c(sapply(1:lag, function(p) sprintf("%s_l%i", var_names, p)))) := sapply(1:lag, function(p) data.table::shift(.SD, p))]
-    # Dependent variable:
-    Y = as.matrix(data_out[(lag+1):.N,1:K])
-    # Explanatory variables:
-    Z = cbind("constant"=1,as.matrix(data_out[(lag+1):.N,(K+1):ncol(data_out)]))
-
-    # Least-squares estimation ----
-    A = try(solve(crossprod(Z), crossprod(Z,Y)))
-
-    if (class(A)!="try-error") {
-      res = Y - Z %*% A
-      Sigma_res = crossprod(res)/(N-1)
-
-      # Information criteria ----
-      AIC = log(det(Sigma_res)) + (2/(N-lag)) * ((K ** 2) * lag + K) # Akaike
-      HQC = log(det(Sigma_res)) + (2 * log(log(N-lag)) /(N-lag)) * ((K ** 2) * lag + K) # Hannan-Quinn
-      BIC = log(det(Sigma_res)) + (log(N-lag)/(N-lag)) * ((K ** 2) * lag + K) # Schwarze/Bayes
-      criteria = data.table::data.table(m = lag,
-                            BIC = BIC,
-                            HQC = HQC,
-                            AIC = AIC)
-    } else {
-      criteria = data.table::data.table(m=lag,
-                            BIC=NA,
-                            HQC=NA,
-                            AIC=NA)
     }
-
-
-  })
+  )
 
   # Bundle output ----
   criteria_overview = data.table::rbindlist(output)
